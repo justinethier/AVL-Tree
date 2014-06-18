fee5c76dcdee6e68eeb5d2699fed8037f34e2921
Justin Ethier <justin.ethier@gmail.com>
2014-06-18 13:42:09 -0400

    Removed tab chars

--- AvlTree.java
+++ AvlTree.java
@@ -19,299 +19,299 @@ class AvlTree<T extends Comparable<? super T>> {
    * @author Justin Ethier
    */
   protected static class AvlNode<T> {
-  	
-  	/**
-  	 * Node data
-  	 */
-  	protected T	element;
-  	
-  	/**
-  	 * Left child
-  	 */
-  	protected AvlNode<T>		left;
-  	
-  	/**
-  	 * Right child
-  	 */
-  	protected AvlNode<T>		right;
-  	
-  	/**
-  	 * Height of node
-  	 */
-  	protected int			height;
-  	
-  	/**
-  	 * Constructor; creates a node without any children
-  	 * 
-  	 * @param theElement	The element contained in this node
-  	 */
-  	public AvlNode (T theElement){
-  		this (theElement, null, null);
-  	}
-  	
-  	/**
-  	 * Constructor; creates a node with children
-  	 * 
-  	 * @param theElement	The element contained in this node
-  	 * @param lt			Left child		
-  	 * @param rt			Right child
-  	 */
-  	public AvlNode (T theElement, AvlNode<T> lt, AvlNode<T> rt){
-  		element = theElement;
-  		left = lt;
-  		right = rt;
-  	}
+    
+    /**
+     * Node data
+     */
+    protected T  element;
+    
+    /**
+     * Left child
+     */
+    protected AvlNode<T>    left;
+    
+    /**
+     * Right child
+     */
+    protected AvlNode<T>    right;
+    
+    /**
+     * Height of node
+     */
+    protected int      height;
+    
+    /**
+     * Constructor; creates a node without any children
+     * 
+     * @param theElement  The element contained in this node
+     */
+    public AvlNode (T theElement){
+      this (theElement, null, null);
+    }
+    
+    /**
+     * Constructor; creates a node with children
+     * 
+     * @param theElement  The element contained in this node
+     * @param lt      Left child    
+     * @param rt      Right child
+     */
+    public AvlNode (T theElement, AvlNode<T> lt, AvlNode<T> rt){
+      element = theElement;
+      left = lt;
+      right = rt;
+    }
   }
-	private AvlNode<T> root;
-	
-	// TODO: make these optional based on some sort of 'debug' flag?
-	// at the very least, make them read-only properties
-	public int countInsertions;
-	public int countSingleRotations;
-	public int countDoubleRotations;
-	
-	/**
-	 * Avl Tree Constructor.
-	 * 
-	 * Creates an empty tree
-	 */
-	public AvlTree (){
-		root = null;
-				
-		countInsertions = 0;
-		countSingleRotations = 0;
-		countDoubleRotations = 0;		
-	}
-	
-	/**
-	 * Determine the height of the given node.
-	 * 
-	 * @param t Node
-	 * @return Height of the given node.
-	 */
-	public int height (AvlNode<T> t){
-		return t == null ? -1 : t.height;
-	}
-	
-	/**
-	 * Find the maximum value among the given numbers.
-	 * 
-	 * @param a First number
-	 * @param b Second number
-	 * @return Maximum value
-	 */	
-	public int max (int a, int b){
-		if (a > b)
-			return a;
-		return b;
-	}
-	
-	/**
-	 * Insert an element into the tree.
-	 * 
-	 * @param x Element to insert into the tree
-	 * @return True - Success, the Element was added. 
-	 *         False - Error, the element was a duplicate.
-	 */
-	public boolean insert (T x){
-		try {
-			root = insert (x, root);
-			
-			countInsertions++;
-			return true;
-		} catch(Exception e){ // TODO: catch a DuplicateValueException instead!
-			return false;
-		}
-	}
-	
-	/**
-	 * Internal method to perform an actual insertion.
-	 * 
-	 * @param x Element to add
-	 * @param t Root of the tree
-	 * @return New root of the tree
-	 * @throws Exception 
-	 */
-	protected AvlNode<T> insert (T x, AvlNode<T> t) throws Exception{
-		if (t == null)
-			t = new AvlNode<T> (x);
-		else if (x.compareTo (t.element) < 0){
-			t.left = insert (x, t.left);
-			
-			if (height (t.left) - height (t.right) == 2){
-				if (x.compareTo (t.left.element) < 0){
-					t = rotateWithLeftChild (t);
-					countSingleRotations++;
-				}
-				else {
-					t = doubleWithLeftChild (t);
-					countDoubleRotations++;
-				}
-			}
-		}
-		else if (x.compareTo (t.element) > 0){
-			t.right = insert (x, t.right);
-			
-			if ( height (t.right) - height (t.left) == 2)
-				if (x.compareTo (t.right.element) > 0){
-					t = rotateWithRightChild (t);
-					countSingleRotations++;
-				}
-				else{
-					t = doubleWithRightChild (t);
-					countDoubleRotations++;
-				}
-		}
-		else {
-			throw new Exception("Attempting to insert duplicate value");
-		}
-		
-		t.height = max (height (t.left), height (t.right)) + 1;
-		return t;
-	}
-	
-	/**
-	 * Rotate binary tree node with left child.
-	 * For AVL trees, this is a single rotation for case 1.
-	 * Update heights, then return new root.
-	 * 
-	 * @param k2 Root of tree we are rotating
-	 * @return New root
-	 */
-	protected AvlNode<T> rotateWithLeftChild (AvlNode<T> k2){
-		AvlNode<T> k1 = k2.left;
-		
-		k2.left = k1.right;
-		k1.right = k2;
-		
-		k2.height = max (height (k2.left), height (k2.right)) + 1;
-		k1.height = max (height (k1.left), k2.height) + 1;
-		
-		return (k1);
-	}
-	
-	/**
-	 * Double rotate binary tree node: first left child
-	 * with its right child; then node k3 with new left child.
-	 * For AVL trees, this is a double rotation for case 2.
-	 * Update heights, then return new root.
-	 * 
-	 * @param k3 Root of tree we are rotating
-	 * @return New root
-	 */
-	protected AvlNode<T> doubleWithLeftChild (AvlNode<T> k3){
-		k3.left = rotateWithRightChild (k3.left);
-		return rotateWithLeftChild (k3);
-	}
-	
-	/**
-	 * Rotate binary tree node with right child.
-	 * For AVL trees, this is a single rotation for case 4.
-	 * Update heights, then return new root.
-	 * 
-	 * @param k1 Root of tree we are rotating.
-	 * @return New root
-	 */
-	protected AvlNode<T> rotateWithRightChild (AvlNode<T> k1){
-		AvlNode<T> k2 = k1.right;
-		
-		k1.right = k2.left;
-		k2.left = k1;
-		
-		k1.height = max (height (k1.left), height (k1.right)) + 1;
-		k2.height = max (height (k2.right), k1.height) + 1;
-		
-		return (k2);
-	}
+  private AvlNode<T> root;
+  
+  // TODO: make these optional based on some sort of 'debug' flag?
+  // at the very least, make them read-only properties
+  public int countInsertions;
+  public int countSingleRotations;
+  public int countDoubleRotations;
+  
+  /**
+   * Avl Tree Constructor.
+   * 
+   * Creates an empty tree
+   */
+  public AvlTree (){
+    root = null;
+        
+    countInsertions = 0;
+    countSingleRotations = 0;
+    countDoubleRotations = 0;    
+  }
+  
+  /**
+   * Determine the height of the given node.
+   * 
+   * @param t Node
+   * @return Height of the given node.
+   */
+  public int height (AvlNode<T> t){
+    return t == null ? -1 : t.height;
+  }
+  
+  /**
+   * Find the maximum value among the given numbers.
+   * 
+   * @param a First number
+   * @param b Second number
+   * @return Maximum value
+   */  
+  public int max (int a, int b){
+    if (a > b)
+      return a;
+    return b;
+  }
+  
+  /**
+   * Insert an element into the tree.
+   * 
+   * @param x Element to insert into the tree
+   * @return True - Success, the Element was added. 
+   *         False - Error, the element was a duplicate.
+   */
+  public boolean insert (T x){
+    try {
+      root = insert (x, root);
+      
+      countInsertions++;
+      return true;
+    } catch(Exception e){ // TODO: catch a DuplicateValueException instead!
+      return false;
+    }
+  }
+  
+  /**
+   * Internal method to perform an actual insertion.
+   * 
+   * @param x Element to add
+   * @param t Root of the tree
+   * @return New root of the tree
+   * @throws Exception 
+   */
+  protected AvlNode<T> insert (T x, AvlNode<T> t) throws Exception{
+    if (t == null)
+      t = new AvlNode<T> (x);
+    else if (x.compareTo (t.element) < 0){
+      t.left = insert (x, t.left);
+      
+      if (height (t.left) - height (t.right) == 2){
+        if (x.compareTo (t.left.element) < 0){
+          t = rotateWithLeftChild (t);
+          countSingleRotations++;
+        }
+        else {
+          t = doubleWithLeftChild (t);
+          countDoubleRotations++;
+        }
+      }
+    }
+    else if (x.compareTo (t.element) > 0){
+      t.right = insert (x, t.right);
+      
+      if ( height (t.right) - height (t.left) == 2)
+        if (x.compareTo (t.right.element) > 0){
+          t = rotateWithRightChild (t);
+          countSingleRotations++;
+        }
+        else{
+          t = doubleWithRightChild (t);
+          countDoubleRotations++;
+        }
+    }
+    else {
+      throw new Exception("Attempting to insert duplicate value");
+    }
+    
+    t.height = max (height (t.left), height (t.right)) + 1;
+    return t;
+  }
+  
+  /**
+   * Rotate binary tree node with left child.
+   * For AVL trees, this is a single rotation for case 1.
+   * Update heights, then return new root.
+   * 
+   * @param k2 Root of tree we are rotating
+   * @return New root
+   */
+  protected AvlNode<T> rotateWithLeftChild (AvlNode<T> k2){
+    AvlNode<T> k1 = k2.left;
+    
+    k2.left = k1.right;
+    k1.right = k2;
+    
+    k2.height = max (height (k2.left), height (k2.right)) + 1;
+    k1.height = max (height (k1.left), k2.height) + 1;
+    
+    return (k1);
+  }
+  
+  /**
+   * Double rotate binary tree node: first left child
+   * with its right child; then node k3 with new left child.
+   * For AVL trees, this is a double rotation for case 2.
+   * Update heights, then return new root.
+   * 
+   * @param k3 Root of tree we are rotating
+   * @return New root
+   */
+  protected AvlNode<T> doubleWithLeftChild (AvlNode<T> k3){
+    k3.left = rotateWithRightChild (k3.left);
+    return rotateWithLeftChild (k3);
+  }
+  
+  /**
+   * Rotate binary tree node with right child.
+   * For AVL trees, this is a single rotation for case 4.
+   * Update heights, then return new root.
+   * 
+   * @param k1 Root of tree we are rotating.
+   * @return New root
+   */
+  protected AvlNode<T> rotateWithRightChild (AvlNode<T> k1){
+    AvlNode<T> k2 = k1.right;
+    
+    k1.right = k2.left;
+    k2.left = k1;
+    
+    k1.height = max (height (k1.left), height (k1.right)) + 1;
+    k2.height = max (height (k2.right), k1.height) + 1;
+    
+    return (k2);
+  }
 
-	/**
-	 * Double rotate binary tree node: first right child
-	 * with its left child; then node k1 with new right child.
-	 * For AVL trees, this is a double rotation for case 3.
-	 * Update heights, then return new root.
-	 * 
-	 * @param k1 Root of tree we are rotating
-	 * @return New root
-	 */
-	protected AvlNode<T> doubleWithRightChild (AvlNode<T> k1){
-		k1.right = rotateWithLeftChild (k1.right);
-		return rotateWithRightChild (k1);
-	}
+  /**
+   * Double rotate binary tree node: first right child
+   * with its left child; then node k1 with new right child.
+   * For AVL trees, this is a double rotation for case 3.
+   * Update heights, then return new root.
+   * 
+   * @param k1 Root of tree we are rotating
+   * @return New root
+   */
+  protected AvlNode<T> doubleWithRightChild (AvlNode<T> k1){
+    k1.right = rotateWithLeftChild (k1.right);
+    return rotateWithRightChild (k1);
+  }
 
 
-	/**
-	 * Serialize the tree to a string using an infix traversal.
-	 * 
-	 * In other words, the tree items will be serialized in numeric order. 
-	 * 
-	 * @return String representation of the tree
-	 */
-	public String serializeInfix(){
-		StringBuilder str = new StringBuilder();
-		serializeInfix (root, str, " ");
-		return str.toString();
-	}
+  /**
+   * Serialize the tree to a string using an infix traversal.
+   * 
+   * In other words, the tree items will be serialized in numeric order. 
+   * 
+   * @return String representation of the tree
+   */
+  public String serializeInfix(){
+    StringBuilder str = new StringBuilder();
+    serializeInfix (root, str, " ");
+    return str.toString();
+  }
 
-	/**
-	 * Internal method to infix-serialize a tree.
-	 * 
-	 * @param t		Tree node to traverse
-	 * @param str	Accumulator; string to keep appending items to.
-	 */
-	protected void serializeInfix(AvlNode<T> t, StringBuilder str, String sep){
-		if (t != null){
-			serializeInfix (t.left, str, sep);
-			str.append(t.element.toString());
-			str.append(sep);
-			serializeInfix (t.right, str, sep);
-		}		
-	}
-	
-	/**
-	 * Serialize the tree to a string using a prefix traversal.
-	 * 
-	 * In other words, the tree items will be serialized in the order that
-	 * they are stored within the tree. 
-	 * 
-	 * @return String representation of the tree
-	 */	
-	public String serializePrefix(){
-		StringBuilder str = new StringBuilder();
-		serializePrefix (root, str, " ");
-		return str.toString();
-	}
-	
-	/**
-	 * Internal method to prefix-serialize a tree.
-	 * 
-	 * @param t		Tree node to traverse
-	 * @param str	Accumulator; string to keep appending items to.
-	 */	
-	private void serializePrefix (AvlNode<T> t, StringBuilder str, String sep){
-		if (t != null){
-			str.append(t.element.toString());
-			str.append(sep);
-			serializePrefix (t.left, str, sep);
-			serializePrefix (t.right, str, sep);
-		}
-	}
-	
-	/**
-	 * Deletes all nodes from the tree.
-	 *
-	 */
-	public void makeEmpty(){
-		root = null;
-	}
-	
-	/**
-	 * Determine if the tree is empty.
-	 * 
-	 * @return True if the tree is empty 
-	 */
-	public boolean isEmpty(){
-		return (root == null);
-	}
+  /**
+   * Internal method to infix-serialize a tree.
+   * 
+   * @param t    Tree node to traverse
+   * @param str  Accumulator; string to keep appending items to.
+   */
+  protected void serializeInfix(AvlNode<T> t, StringBuilder str, String sep){
+    if (t != null){
+      serializeInfix (t.left, str, sep);
+      str.append(t.element.toString());
+      str.append(sep);
+      serializeInfix (t.right, str, sep);
+    }    
+  }
+  
+  /**
+   * Serialize the tree to a string using a prefix traversal.
+   * 
+   * In other words, the tree items will be serialized in the order that
+   * they are stored within the tree. 
+   * 
+   * @return String representation of the tree
+   */  
+  public String serializePrefix(){
+    StringBuilder str = new StringBuilder();
+    serializePrefix (root, str, " ");
+    return str.toString();
+  }
+  
+  /**
+   * Internal method to prefix-serialize a tree.
+   * 
+   * @param t    Tree node to traverse
+   * @param str  Accumulator; string to keep appending items to.
+   */  
+  private void serializePrefix (AvlNode<T> t, StringBuilder str, String sep){
+    if (t != null){
+      str.append(t.element.toString());
+      str.append(sep);
+      serializePrefix (t.left, str, sep);
+      serializePrefix (t.right, str, sep);
+    }
+  }
+  
+  /**
+   * Deletes all nodes from the tree.
+   *
+   */
+  public void makeEmpty(){
+    root = null;
+  }
+  
+  /**
+   * Determine if the tree is empty.
+   * 
+   * @return True if the tree is empty 
+   */
+  public boolean isEmpty(){
+    return (root == null);
+  }
 
 
 
@@ -449,7 +449,7 @@ class AvlTree<T extends Comparable<? super T>> {
    * @return True if the element is found, false otherwise
    */
   public boolean find(T x){ // TODO: would be more useful to store key/value,
-	                                   // and use key to perform the lookup here...
+                                     // and use key to perform the lookup here...
     return find(x, root); 
   }
 
@@ -472,26 +472,26 @@ class AvlTree<T extends Comparable<? super T>> {
 
     return true; // Can only reach here if node was found
   }
-	
-	/**
-	 * Main entry point; contains test code for the tree.
-	 */
-	public static void main () { //String []args){
-		AvlTree<Integer> t = new AvlTree<Integer>();
-		
-		t.insert (new Integer(2));
-		t.insert (new Integer(1));
-		t.insert (new Integer(4));
-		t.insert (new Integer(5));
-		t.insert (new Integer(9));
-		t.insert (new Integer(3));
-		t.insert (new Integer(6));
-		t.insert (new Integer(7));
-		
-		System.out.println ("Infix Traversal:");
-		System.out.println(t.serializeInfix());
-		
-		System.out.println ("Prefix Traversal:");
-		System.out.println(t.serializePrefix());
-	}
+  
+  /**
+   * Main entry point; contains test code for the tree.
+   */
+  public static void main () { //String []args){
+    AvlTree<Integer> t = new AvlTree<Integer>();
+    
+    t.insert (new Integer(2));
+    t.insert (new Integer(1));
+    t.insert (new Integer(4));
+    t.insert (new Integer(5));
+    t.insert (new Integer(9));
+    t.insert (new Integer(3));
+    t.insert (new Integer(6));
+    t.insert (new Integer(7));
+    
+    System.out.println ("Infix Traversal:");
+    System.out.println(t.serializeInfix());
+    
+    System.out.println ("Prefix Traversal:");
+    System.out.println(t.serializePrefix());
+  }
 }
--- Test.java
+++ Test.java
@@ -1,14 +1,14 @@
 import java.util.Random;
 
 /* snippet of test code, and a potential fix?
-	import static org.junit.Assert.*;
+  import static org.junit.Assert.*;
  
 import org.junit.Test;
  
 */
 
 class Test {
-	
+  
     private AvlTree<Integer> tree = new AvlTree<Integer>();
  
     private void insert(Integer...integers) {
@@ -125,113 +125,57 @@ class Test {
     }
 }
 */
-	/**
-	 * Test method to perform a series of 100 unique insertions into 
-	 * a blank AVL Tree, and returns the tree.
-	 * 
-	 * TODO: this code would be better served in another Test class.
-	 * 
-	 * 
-	 * @return Tree with insertions
-	 */
-	public static void performInsertions(AvlTree<Integer> t){
-		Random r = new Random();
-		int range = 500;
-		int count = 100;
-		Integer x;				
-		
-		// Delete any old nodes from the tree
-		t.makeEmpty();
-		
-		// Clear the counts
-		t.countInsertions = 0;
-		t.countSingleRotations = 0;
-		t.countDoubleRotations = 0;
-		
-		// Generate and insert 100 random numbers
-		for (int i = 0; i < count; i++){
-			// Prevent insertion of duplicates
-			//
-			//  Note: must take care in selecting parameters,
-			//        to avoid an infinite loop. If count > max - min,
-			//        then we have a problem.
-			do {
-				x = new Integer (r.nextInt(range) + 1);
-			} while (!t.insert (x));
-		}
-	}
-	
-	public static void main (String []args){
-		AvlTree<Integer> t = new AvlTree<Integer>();
-		int testCases = 10;
-		int insertionCount = 0;
-		int singleRotationCount = 0;
-		int doubleRotationCount = 0;
-		
-		System.out.println ("Perfoming a series of " + testCases + " test cases.");
-		System.out.println ("\nSR\tDR\n");
-		
-		for (int i=0; i < testCases; i++) {
-			Test.performInsertions(t);
-			insertionCount      += t.countInsertions;
-			singleRotationCount += t.countSingleRotations;
-			doubleRotationCount += t.countDoubleRotations;
-			
-			System.out.println (t.countSingleRotations + "\t" + t.countDoubleRotations);
-		}
-		
-		System.out.println ("Total Insertions:       " + insertionCount);
-		System.out.println ("Total Single Rotations: " + singleRotationCount);
-		System.out.println ("Total Double Rotations: " + doubleRotationCount);
-		
-		t.makeEmpty();
-		
-		t.insert (new Integer(2));
-		t.insert (new Integer(1));
-		t.insert (new Integer(4));
-		t.insert (new Integer(5));
-		t.insert (new Integer(9));
-		t.insert (new Integer(3));
-		t.insert (new Integer(6));
-		t.insert (new Integer(7));
-		
-		System.out.println ("Infix Traversal:");
-		System.out.println (t.serializeInfix());
-		
-		System.out.println ("Prefix Traversal:");
-		System.out.println (t.serializePrefix());
+  /**
+   * Test method to perform a series of 100 unique insertions into 
+   * a blank AVL Tree, and returns the tree.
+   * 
+   * TODO: this code would be better served in another Test class.
+   * 
+   * 
+   * @return Tree with insertions
+   */
+  public static void performInsertions(AvlTree<Integer> t){
+    Random r = new Random();
+    int range = 9999999;
+    int count = 9999999;
+    Integer x;        
+    
+    // Delete any old nodes from the tree
+    t.makeEmpty();
+    
+    // Clear the counts
+    t.countInsertions = 0;
+    t.countSingleRotations = 0;
+    t.countDoubleRotations = 0;
+    
+    // Generate and insert 100 random numbers
+    for (int i = 0; i < count; i++){
+      // Prevent insertion of duplicates
+      //
+      //  Note: must take care in selecting parameters,
+      //        to avoid an infinite loop. If count > max - min,
+      //        then we have a problem.
+      //do {
+        x = new Integer (r.nextInt(range) + 1);
+            t.insert(x);
+      //} while (!t.insert (x));
+    }
+  }
   
-	    // Ensure only the numbers above are found in the tree 
-	    for (int i = 1; i < 8; i++){ 
-	      assert (t.find(i)) : i + " is not found";
-	    }
-	    assert (t.find(9)) : "9 is not found";
-	
-	    for (int i = 10; i < 18; i++){ 
-	      assert (!t.find(i)) : i + " is found";
-	    }
-
-	    assert (!t.insert(new Integer(9))) : "Duplicate 9 was inserted";
-
-		  t.makeEmpty();		
-  		t.insert (new Integer(2));
-      t.remove (2);
-      assert (t.isEmpty()) : "2 was not removed from tree";
-
-		  t.makeEmpty();		
-  		t.insert (new Integer(11));
-  		t.insert (new Integer(22));
-      t.remove (11);
-		System.out.println ("Prefix Traversal:");
-		System.out.println (t.serializePrefix());      
-      assert (!t.find(11)) : "11 was not removed from tree";
-
-		  t.makeEmpty();		
-  		t.insert (new Integer(11));
-  		t.insert (new Integer(22));
-      t.remove (22);
-		System.out.println ("Prefix Traversal:");
-		System.out.println (t.serializePrefix());      
-      assert (!t.find(22)) : "22 was not removed from tree";
-	}
+  public static void main (String []args){
+    AvlTree<Integer> t = new AvlTree<Integer>();
+    int testCases = 10;
+    int insertionCount = 0;
+    int singleRotationCount = 0;
+    int doubleRotationCount = 0;
+    
+      Test.performInsertions(t);
+      insertionCount      += t.countInsertions;
+      singleRotationCount += t.countSingleRotations;
+      doubleRotationCount += t.countDoubleRotations;
+    
+    System.out.println ("Total Insertions:       " + insertionCount);
+    System.out.println ("Total Single Rotations: " + singleRotationCount);
+    System.out.println ("Total Double Rotations: " + doubleRotationCount);
+  }
 }
